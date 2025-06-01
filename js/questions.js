638577429183bf65a6e3ca3cd25b7db8814a5001
vesrntechnologies <vennjoy@gmail.com>
2025-06-01 21:03:16 +0530

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -12,7 +12,114 @@ let questions = [
       "Removes unnecessary items"
     ]
   },
- 
+ {
+    numb: 2,
+    question: "What is the use of try square shown in the figure?",
+    immg:"img2",
+    answer: "To check the squareness",
+    options: [
+      "To check the flatness",
+      "To check the squareness",
+      "To mark lines at 90o", 
+      "To set the workpiece at right angle"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the use of groove provided on the either side of a vee-block?",
+    
+    answer: "Holding clamps",
+    options: [
+      "For appearance",
+      "For reducing the weight",
+      "To reduce bearing surface",
+      "Holding clamps"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which metal property can be drawn into the wire without any rupture?",
+   // immg:"img4",
+    answer: "Ductility",
+    options: [
+      "Ductility",
+      "Tenacity",
+      "Elasticity",
+      "Malleability"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Why slots are provided in the slotted angle plate?",
+    answer: "For job clamping",
+    options: [
+      "For job clamping",
+      "For easy handling",
+      "For reducing the weight",
+      "For better appearance"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which metal property can resist the effect of tensile forces without any rupture?",
+    //immg:"img6",
+    answer: "Tenacity",
+    options: [
+      "Ductility",
+      "Tenacity",
+      "Elasticity",
+      "Malleability"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the name of the parallel block shown in the figure?",
+      immg:"img7",
+    answer: "Adjustable parallel block",
+    options: [
+      "Solid parallel block",
+      "Tenon parallel block,
+      "Angular parallel block",
+      "Adjustable parallel block"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which marking media is applied in rough forgings and castings?",
+    //immg:"img8",
+    answer: "White wash",
+    options: [
+      "White wash",
+      "Prussian blue",
+      "Copper Sulphate",
+      "Cellulose lacquer"
+    ]
+  },
+      {
+    numb: 9,
+    question: "Which metal cutting saw is used for cutting different profiles?",
+   // immg:"img9",    
+    answer: "Contour band saw",
+    options: [
+      "Power saw",
+      "Circular saw",
+      "Contour band saw",
+      "Horizontal band saw"
+    ]
+  },
+    {
+    numb: 10,
+    question: "Which metal property permits no permanent distortion on before breaking?",
+    
+    answer: "Brittleness",
+    options: [
+      "Tenacity",
+      "Hardness",
+      "Brittleness",
+      "Toughness"
+    ]
+  },
+     
   // you can uncomment the below codes and make duplicate as more as you want to add question
   // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
 
